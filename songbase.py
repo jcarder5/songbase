d090e752ac75e0e616b31913e97fb0a4b9c38029
James Carder <jcarder@udel.edu>
2017-11-09 13:47:48 -0500

    flask stuff

--- songbase.py
+++ songbase.py
@@ -1,18 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return '<h1>hello world!!!!!</h1>'
+    #return '<h1>hello world!!!!!</h1>'
+    return render_template('idex.html')
 
 
 @app.route('/user/<string:name>/')
 def get_user(name):
-    return '<h1>hello %s</h1>' % name
+    #return '<h1>hello %s your age is %d</h1>' % (name, 3)
+    return render_template('user.html', user_name = name)
 
 
+@app.route('/songs')
+def list_all_songs():
 
+    songs = [
+    'song 1'
+    'song 2'
+    'song 3'
+    ]
+    return render_template('songs.html', songs=songs)
 
 if __name__ == '__main__':
-    app.run(debug==True)
+    app.run()
